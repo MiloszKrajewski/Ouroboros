f77213dec1f5a6491246a04ea05a9703c3a5f8d4
Milosz Krajewski <krajewskim@SEPURA1166.SEPURA.co.uk>
2016-05-03 10:50:18 +0100

    added ProjectScaffold

---
+++ src/Ouroboros/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Ouroboros")>]
+[<assembly: AssemblyProductAttribute("Ouroboros")>]
+[<assembly: AssemblyDescriptionAttribute("distributed EvoLisa with Akka.net")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
+    let [<Literal>] InformationalVersion = "1.0"
